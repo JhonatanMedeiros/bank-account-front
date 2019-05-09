ac9f6d35a95cbe41a0f0a94d1c0b0377105dd965
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2019-05-08 23:24:42 -0300

    chore(protractor/tslint): change protractor config and update tslint rules

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -3,24 +3,36 @@
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
+const CHROME_OPTIONS = {
+  'args': ['--js-flags=--expose-gc', '--no-sandbox', '--headless', '--disable-dev-shm-usage'],
+  'perfLoggingPrefs': {
+    'traceCategories':
+      'v8,blink.console,devtools.timeline,disabled-by-default-devtools.timeline,blink.user_timing'
+  }
+};
+
 exports.config = {
   allScriptsTimeout: 11000,
+  useAllAngular2AppRoots: true,
+  restartBrowserBetweenTests: true,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome',
-    'chromeOptions': {
-    'args': ['headless']
-  }
+    chromeOptions: CHROME_OPTIONS,
+    loggingPrefs: {
+      performance: 'ALL',
+      browser: 'ALL',
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
+    defaultTimeoutInterval: 60000,
+    print: function(msg) { console.log(msg); },
   },
   onPrepare() {
     require('ts-node').register({
